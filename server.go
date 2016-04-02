ba7bacce45a287861cf9bff6fe599553c01b941e
mono <mono0x@users.noreply.github.com>
2016-04-02 13:50:27 +0900

    Fix

--- server.go
+++ server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/braintree/manners"
 	"github.com/lestrrat/go-server-starter/listener"
@@ -62,4 +64,41 @@ func (s *Server) Run(args []string) int {
 
 func (s *Server) Synopsis() string {
 	return `Start server`
+}
+
+func handleIndex(c *appContext, w http.ResponseWriter, r *http.Request) {
+	var ranking Ranking
+	if err := c.dbMap.SelectOne(&ranking,
+		`SELECT * FROM ranking WHERE started_on <= :today ORDER BY ended_on DESC, started_on DESC LIMIT 1`,
+		map[string]interface{}{
+			"today": time.Now(),
+		}); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var rankingItems []RankingItem
+	if _, err := c.dbMap.Select(&rankingItems, `
+		SELECT
+			character.name AS Name,
+			x.count AS Count
+		FROM (
+			SELECT
+				character_id, COUNT(*) AS count
+			FROM vote
+			WHERE vote.ranking_id = :ranking_id
+			GROUP BY vote.character_id
+		) x
+		JOIN character ON character.id = x.character_id
+		ORDER BY x.count DESC, character.name ASC
+	`, map[string]interface{}{
+		"ranking_id": ranking.Id,
+	}); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	_ = renderLayout(w, "Sanrio Character Ranking Viewer", func(w io.Writer) error {
+		return renderIndex(w, ranking, rankingItems)
+	})
 }
